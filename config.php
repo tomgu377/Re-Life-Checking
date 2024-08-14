8e7f6e122a74b00883a4fafee509c7be6a52770f
tomgu377 <tomgu377@gmail.com>
2024-08-14 11:19:03 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+$config = [
+    /* [ LISENCE APIKEY CONFIGURATION ] */
+    'apikey'            => 'Apikey Kamu',
+
+    /* [ PROXY WITH USER:PASS CONFIGURATION ] */
+    // 1
+    'proxyauth'         => false,                           // hidup/matikan penggunaan banyak daftar ip proxy
+    'proxyuser'         => 'USER:PW',                       // kredensial proxy
+    'proxylist'         => 'proxy.txt',                     // gunakan banyak daftar ip proxy
+
+    /* [ PROXY WITH WITHOUT USER:PASS AUTH CONFIGURATION ] */
+    // 2
+    'proxyserver'       => false,                           // hidup/matikan penggunaan 1 koneksi proxy
+    'proxy_auth'        => false,
+    'proxyauth2'        => 'USER:PW',
+    'serverorip_proxy'  => 'ip/domain:port or file.txt',    // gunakan 1 koneksi ke server proxy
+
+    /* [ PROGRAM CONFIGURATION PROCESSING ] */
+    'proxytype'         => 'socks5',                // proxy tipe. pilih: HTTP / HTTP_1_0 / HTTPS / SOCKS4 / SOCKS4A / SOCKS5_HOSTNAME / SOCKS5
+    'thread'            => 2,                       // kecepatan proses bot per detik
+    'delay'             => 3,                       // jeda program berjalan
+    'display'           => true,                    // menampilkan berjalannya bot
+
+    /* [ APLICATION CONFIGURATION ] */
+    'appversion'        => '39.0.0.1',              // versi aplikasi
+    'checkingmode'      => '',                      // full: untuk pengecekkan keseluruhan sampai dengan balance di akun, email: hanya cek email terdaftar/tidak
+
+    /* [ SAVE CONFIGURATION ] */
+    'save_on_internal'  => false,                   // hasil akan disave pada local folder
+    'save_on_external'  => false,                   // hasil akan disave pada spreadsheet
+    'external_link_tosave' => 'link spreadsheet'    // google spreadsheet app script link
+];
