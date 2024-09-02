23af4d5b2ab727894720f071a34f5717e967f8c9
tomgu377 <tomgu377@gmail.com>
2024-09-02 21:06:26 +0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -7,10 +7,11 @@ do
     # Cek apakah ada file dengan ekstensi .txt di dalam folder
     if compgen -G "$folder/*.txt" > /dev/null; then
         # Jika ada file .txt, jalankan program PHP tanpa argumen file
-        php run.php
+        php index.php
     else
         # Jika tidak ada file .txt, hentikan script
-        echo "Tidak ada file .txt di dalam folder $folder. Bot dihentikan."
+        php index.php
+        echo "[!] Tidak ada file .txt di dalam folder $folder. Re-Life Checking dihentikan."
         break
     fi
 
